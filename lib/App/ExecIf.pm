6f35d94cd78dd86860e9992724ce79e2ab4a8418
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2013-12-25 20:18:18 +0700

    Initial commit

---
+++ lib/App/ExecIf.pm
@@ -0,0 +1,15 @@
+package App::ExecIf;
+
+# VERSION
+
+1;
+# ABSTRACT: Execute command on some condition
+
+=head1 SYNOPSIS
+
+This distribution provides some command-line utilities:
+
+ exec-if-env
+ exec-if-not-env
+
+=cut
